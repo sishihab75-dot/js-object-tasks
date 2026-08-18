58f43b89ca95747eedd6addc00987e58868ab3e5
sishihab75-dot <sishihab75@gmail.com>
2026-08-18 23:52:43 +0600

    complite task_5

---
+++ tasl_5.js
@@ -0,0 +1,14 @@
+let myObject = {
+
+    name: 'John Doe',
+    age: 25,
+    city: 'Example City',
+    isStudent: true
+
+};
+let count = 0;
+for (let key in myObject) {
+  count++;
+}
+
+console.log(myObject);
