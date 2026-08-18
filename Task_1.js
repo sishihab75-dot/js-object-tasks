22d9596546cfa5695660bd55dd67f995f0ab9003
sishihab75-dot <sishihab75@gmail.com>
2026-08-18 23:25:03 +0600

    complite task 1

---
+++ Task_1.js
@@ -0,0 +1,16 @@
+const colors = {
+    red: "#ff0000",
+    green: "#00ff00",
+    blue: "#0000ff",
+    golden_rod: "#daa520"
+};
+console.log(colors.golden_rod);
+
+
+const Colors = {
+    red: "#ff0000",
+    green: "#00ff00",
+    blue: "#0000ff",
+    "golden rod": '#daa520'
+};
+console.log(Colors["golden rod"]);
